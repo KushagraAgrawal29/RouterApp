a417a35b4549a4505af2746f96055fa27df90d20
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2023-12-22 23:26:15 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./Components/PrivateRoute"; 
+import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+// import PrivateRoute from "./Components/PrivateRoute";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div className="w-screen h-screen bg-richblack-900 flex flex-col">
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+      <Routes>
+
+        <Route path="/" element={<Home setIsLoggedIn={setIsLoggedIn}/>}></Route>
+        <Route path="login" element={<Login setIsLoggedIn={setIsLoggedIn}/>}></Route>
+        <Route path="signup" element={<Signup setIsLoggedIn={setIsLoggedIn}/>}></Route>
+        <Route path="dashboard" element={
+        <PrivateRoute isLoggedIn={isLoggedIn}>
+          <Dashboard/>
+        </PrivateRoute>
+        }></Route>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      {/* Welcome to study notion */}
+    </div>
+  )
+}
+
+export default Dashboard
